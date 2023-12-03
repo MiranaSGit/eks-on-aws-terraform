a94a1bc1f5590e86765a5850d0ae0b9f46e700e9
Ömer Faruk Demirözü <odemiro@corp.vodafone.es>
2023-12-03 20:02:52 +0300

    .

---
+++ eks-fargate-only-cluster/0-providers.tf
@@ -0,0 +1,29 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      #version = ">= 3.63"
+      version = ">= 4.65"      
+     }
+  }
+
+  # Adding Backend as S3 for Remote State Storage
+  # backend "s3" {
+  #   bucket = "terraform-states-ofd"
+  #   key    = "eks-fargate-only.terraform.tfstate"
+  #   region = "us-east-1"
+  #   profile = "omer" 
+ 
+  #   # For State Locking
+  #   dynamodb_table = "eks-fargate-only-table"    
+  # }  
+}  
+
+
+# Terraform AWS Provider Block
+provider "aws" {
+    region = "us-east-1"
+    profile = "omer"
+}
