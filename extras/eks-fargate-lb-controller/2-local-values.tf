ec3cbddd3469f6b18d8e3dd9b531bbe7406eae9b
Ömer Faruk Demirözü <odemiro@corp.vodafone.es>
2023-12-04 17:39:02 +0300

    .

--- extras/eks-fargate-lb-controller/2-local-values.tf
+++ extras/eks-fargate-lb-controller/2-local-values.tf
@@ -1,11 +1,10 @@
 # Define Local Values in Terraform
 locals {
-  owners = var.business_divsion
+  owners = var.project
   environment = var.environment
-  name = "${var.business_divsion}-${var.environment}"
+  name = "${var.environment}-${var.cluster_name}"
   common_tags = {
     owners = local.owners
     environment = local.environment
   }
-  eks_cluster_name = "${data.terraform_remote_state.eks.outputs.cluster_id}"  
-} 
+}
