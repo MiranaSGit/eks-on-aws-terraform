5ce88a55aa3831c1de2fb2039c335cb67dceeb5d
Ömer Faruk Demirözü <odemiro@corp.vodafone.es>
2023-12-05 12:22:26 +0300

    ,

--- eks/0-providers.tf
+++ eks/0-providers.tf
@@ -27,3 +27,15 @@ provider "aws" {
     region = "us-east-1"
     profile = "omer"
 }
+
+# Datasource: 
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.eks_cluster.id
+}
+
+# Terraform Kubernetes Provider
+provider "kubernetes" {
+  host = aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  token = data.aws_eks_cluster_auth.cluster.token
+}
